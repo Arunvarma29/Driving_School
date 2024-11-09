73e740a6ae7cae4724dd64c4ca67808395be6666
Arunvarma29 <varmaarun580@gmail.com>
2024-11-09 22:36:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Oswald':['Oswald' , 'sans-serif'],
+        'Courgette':['Courgette','sans-serif'],
+        'Montserrat':['Montserrat','sans-serif'],
+        'Open-sans':['Open-sans','sans-serif']
+
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
